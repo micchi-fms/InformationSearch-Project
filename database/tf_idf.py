59064887b22f5088a3cc551b71fd905ef474f65b
micchi-fms <tmchisuper777@gmail.com>
2018-11-08 15:29:52 +0900

    multiprocessing program

--- database/tf_idf.py
+++ database/tf_idf.py
@@ -4,7 +4,12 @@ import mysql.connector
 import configparser
 import MeCab
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer    
+from sklearn.feature_extraction.text import TfidfVectorizer
+# from sklearn.externals.joblib import Parallel, delayed
+# from joblib import Parallel, delayed
+from multiprocessing import Pool
+import multiprocessing as multi
+
 
 config = configparser.ConfigParser()
 # 設定ファイルを読み込み
@@ -24,29 +29,35 @@ cursor = conn.cursor()
 
 ### MySQL上のデータ取得用関数
 def fetch_target_day_n_random(target_day, n = 2000):
-
-    sql = 'select dialogue from origin where date= %s limit %s;'
-    cursor.execute(sql, [target_day,n])
+    sql = 'select dialogue from origin partition (p%s) where date_format(date, "%Y") = "%s";'
+    # sql = 'select dialogue from origin where date_format(date, "%Y") = %s;'
+    cursor.execute(sql, [target_day,target_day])
     result = cursor.fetchall()
     l = [x[0] for x in result]
+    print(l)
     return l
  
 ### MeCab による単語への分割関数 (名詞のみ残す)
+tagger = MeCab.Tagger()
 def split_text_only_noun(text):
-    tagger = MeCab.Tagger()
-    tagger.parse("")
+#    tagger.parse("")
     #text_str = text.encode('utf-8') # str 型じゃないと動作がおかしくなるので変換
-    node = tagger.parseToNode(text)
+#    node = tagger.parseToNode(text)
  
     words = []
-    while node:
-        pos = node.feature.split(",")[0]
-        if pos == "名詞":
-            # unicode 型に戻す
-            #word = node.surface.decode("utf-8")
-            print(node.surface)
-            words.append(node.surface)
-        node = node.next
+    # while node:
+    #     pos = node.feature.split(",")[0]
+    #     if pos == "名詞":
+    #         # unicode 型に戻す
+    #         #word = node.surface.decode("utf-8")
+    #         print(node.surface)
+    #         words.append(node.surface)
+    #     node = node.next
+    for chunk in tagger.parse(text).splitlines()[:-1]:
+        (surface, feature) = chunk.split('\t')
+        if feature.startswith('名詞'):
+            # print(surface)
+            words.append(surface)
     return " ".join(words)
  
 ### TF-IDF の結果からi 番目のドキュメントの特徴的な上位 n 語を取り出す
@@ -59,18 +70,28 @@ def extract_feature_words(terms, tfidfs, i, n):
 ### メイン処理
 docs_count = 20 # 取得数
 target_days = [
-    "2000-01-31",
-    "2000-02-01"
+    1947,
+    1948,
+    1949,
+    1950
 ]
- 
+
 target_day_nouns = []
 for target_day in target_days:
     print(target_day)
     # MySQL からのデータ取得
     txts = fetch_target_day_n_random(target_day, docs_count)
     # 名詞のみ抽出
-    each_nouns = [split_text_only_noun(txt) for txt in txts]
+    # each_nouns = [split_text_only_noun(txt) for txt in txts]
+    # each_nouns = Parallel(n_jobs=-1, pre_dispatch='all')(delayed(split_text_only_noun)(txt) for txt in txts)
+    print('並列処理するよ')
+    p = Pool(multi.cpu_count())
+    each_nouns=p.map(split_text_only_noun, txts)
+    p.close()
+
+    # each_nouns = Parallel(n_jobs=2)(delayed(split_text_only_noun)(txt) for txt in txts)
     all_nouns = " ".join(each_nouns)
+    print(all_nouns)
     target_day_nouns.append(all_nouns)
  
 # TF-IDF 計算
